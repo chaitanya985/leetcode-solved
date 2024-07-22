7a66aa34ba819245523353fceda6b4a6a36e9ffe
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-07-22 12:26:59 +0530

    Time: 103 ms (65.90%) | Memory: 17.2 MB (14.55%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+
+        idx = list(range(len(heights)))
+        
+        idx.sort(key=lambda i: -heights[i])
+        
+        return [names[i] for i in idx]
+        
