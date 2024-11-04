76241c9f4e81c162e59d008b8e4fc6306ec5e377
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-11-04 13:40:59 +0530

    Time: 3231 ms (13.60%) | Memory: 21.7 MB (39.38%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+
+        self.weights=w
+        
+    def pickIndex(self) -> int:
+
+        return random.choices(range(len(self.weights)), weights=self.weights)[0]
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
